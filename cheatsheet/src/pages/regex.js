8193d39355ee2ba76c4d4fdff283c8bb42899636
andrazpolak <andrazpolak@gmail.com>
2023-11-25 14:08:40 +0100

    fds

---
+++ cheatsheet/src/pages/regex.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Regex() {
+  return (
+    <>
+      <Head>
+        <title>Cheat Sheet</title>
+        <meta name="description" content="Cheat sheet for programmers" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Regex</h1>
+    </>
+  );
+}
